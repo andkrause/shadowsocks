bb158e0352f4e75833c643ca6bd0320eaaac7098
andkrause <62185124+andkrause@users.noreply.github.com>
2024-10-29 13:17:33 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 # block all outgoing traffic to local network
 iptables -I INPUT -s 192.168.0.0/16,172.16.0.0/12,10.0.0.0/8 -j DROP
 
-# run parent entrypoint
+# run parent entrypoint 
 ./usr/bin/docker-entrypoint.sh
